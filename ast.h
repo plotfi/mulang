92596a439544268ef2a0d365f91df7967fc226fd
Puyan Lotfi <puyan@puyan.org>
2023-08-17 14:09:02 -0700

    ripping out more C syntax

--- ast.h
+++ ast.h
@@ -2,19 +2,56 @@
 #include <string>
 #include <iostream>
 
-struct NamedDecl {
+using Type = std::string;
+
+struct TypedDecl {
   std::string name;
+  Type type;
+};
+
+struct Decl {
+  virtual std::string str() const = 0;
+};
+
+struct LamdaDecl : public Decl {
+  std::vector<TypedDecl> inputArgs;
+  Type returnType;
+
+  virtual std::string str() const {
+    return "lamda";
+  }
+};
+
+struct Definition {
+  std::string name;
+  Decl *content;
+
+  Definition(std::string name, Decl *content):
+    name(name), content(content) {}
+
+  std::string str() const {
+    return "(define " + name + " " +
+           content->str() +
+           ")";
+  }
 };
 
 struct TranslationUnitDecl {
   std::string name;
-  std::vector<NamedDecl*> children;
+  std::vector<Definition*> children;
 
   void dump() const {
-    std::cout << "TU Name: " << name << "\n";
-    for (auto *ND : children) {
-      std::cout << "\tNamedDecl: " << ND->name << "\n";
-    }
+    for (auto *child : children)
+      std::cout << "\n" << child->str();
   };
 };
 
+
+
+inline std::vector<Definition*> *AppendDecl(void *decls, const char *name) {
+  std::vector<Definition*> *Decls = (std::vector<Definition*>*)decls;
+  if (!Decls)
+    Decls = new std::vector<Definition*>();
+  Decls->push_back(new Definition(std::string(name), nullptr));
+  return Decls;
+}
--- test.mu
+++ test.mu
@@ -1,11 +1,24 @@
+
+/// Syntax Version A:
 define foo = lambda (a: int32) -> int {
   return a + 42
 }
 
+type int = i32
+
+/// Syntax Version B:
+function foo (b: int) -> int {
+  let a : int = 42;
+  return a + b;
+}
+
+/// Structs and typedefs?
 define _S = struct {
   a: int32
   b: float32
 }
 
 define S = type _S
+
+/// Note: Considering dropping structs for now
 
